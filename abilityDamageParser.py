92650f10dad1885a64dfab9b5440ffd8dd3a10b1
J F <cryonicfuse@gmail.com>
2024-07-23 22:20:01 -0700

    Finished result writing functionality

--- abilityDamageParser.py
+++ abilityDamageParser.py
@@ -10,6 +10,9 @@ SETTINGS.json contains the following:
     REPEAT_INTERVAL [int] -         time between repeated events allowed before they are considered to be repeats that should be ignored
                                     // Useful for ignoring spread mechanics which occur concurrently while allowing repeat raidwides through
 Add event names you want to ignore to BLACKLIST.json
+
+RUNTIME: O(nm) where n := # of unique abilities, and m := # of matched events
+assumes that the # of events which match with "prepares" and events which match "U: [0-9]+, M: [0-9]+" is roughly the same (Boss prepares abilities befores resolving)
 """
 import os, re, json
 from dev.log import Log, LogFileNotFound
@@ -37,7 +40,7 @@ def main():
                 print("Log file not found. actionLog could not be initialized. Do you have a file named: \"" + file + "\" in the same directory as this script?")
                 input("Press ENTER to exit...")
                 return -1
-        # parse log
+        # parse log for unique action names
         for event in actionEvents:
             if event in BLACKLIST or event in ACTIONS: continue
             else: ACTIONS.update({event:"0"})
@@ -50,6 +53,22 @@ def main():
             print("Log file not found. damageLog could not be initialized. Do you have a file named: \"" + file + "\" in the same directory as this script?")
             input("Press ENTER to exit...")
             return -1
+        
+        # parse log for max damage
+        for raw_event in damageEvents:
+            for ability in ACTIONS:
+                if re.search(ability, raw_event):
+                    unmitigatedString= re.search("U: \d+", raw_event).group()
+                    unmitigatedDMG = re.search("\d+", unmitigatedString).group()
+                    ACTIONS.update({ability:str(max(ACTIONS[ability], unmitigatedDMG))})
 
+    # output
+    print("{:20} |\t {:7}".format(*["EVENT NAME", "MAX DAMAGE"]))
+    print("{:20} |\t {:7}".format(*["-"*20, "-"*7]))
+    for ability in ACTIONS:
+        if int(ACTIONS[ability]) > 0:
+            print("{:20} |\t {:7}".format(*[ability, ACTIONS[ability]]))
+            results.write(ability +'\t'+ ACTIONS[ability] + '\n')
+    results.close()
 if __name__ == '__main__':
     main()
